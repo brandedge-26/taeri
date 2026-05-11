d5d37923ed4dcf42274d1720d2e6edc10ebb2590
brandedge-26 <digital.bec26@gmail.com>
2026-05-11 14:35:53 +0500

    fix: force ipv4first DNS to fix ENETUNREACH on Railway

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,13 +1,13 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
 import { ENV } from "./env.js";
 
-
+dns.setDefaultResultOrder("ipv4first");
 
 export const mailTransporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    family: 4,
     auth: {
         user: ENV.SENDER_EMAIL,
         pass: ENV.SENDER_PASS
